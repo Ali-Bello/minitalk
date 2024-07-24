242a02a378027d66d09466f0cfe5314c97426866
Bello <m.aliderraj@gmail.com>
2024-07-24 03:04:13 +0100

    40%

--- minitalk.h
+++ minitalk.h
@@ -14,6 +14,8 @@
 # define MINITALK_H
 
 # include <signal.h>
+# include <stdlib.h>
 # include <unistd.h>
+# include "ft_printf/ft_printf.h"
 
 #endif
--- server.c
+++ server.c
@@ -12,8 +12,23 @@
 
 #include "minitalk.h"
 
+void    handle_sigusr1(int signum)
+{
+
+}
+
+void    handle_sigusr2(int signum)
+{
+
+}
+
 int main()
 {
     struct sigaction sa;
-    
+    struct sigaction sa1;
+
+    sa.sa_handler = handle_sigusr1;
+    sa.sa_handler = handle_sigusr2;
+
+    ft_printf("SERVER PID : %d\n", getpid());
 }
